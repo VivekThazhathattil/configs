06aab5ef63b87be8346759c212dfe64647be6575
Vivek Thazhathattil <vivek.thazhathattil@gmail.com>
2020-09-15 16:44:27 +0530

    i3blocks transparency

--- i3/scripts/temp.sh
+++ i3/scripts/temp.sh
@@ -5,7 +5,7 @@ THERM=$(acpi -t | grep -E -o '[0-9][0-9]')
 # Full and short texts
 echo "  $THERM°C "
 
-color=blue
-# Set urgent flags above 50 deg and above 60 deg
-[ ${THERM%?} -ge 50 ] && echo "FF8000"
-[ ${THERM%?} -ge 60 ] && echo "FF8000"
+#color=blue
+## Set urgent flags above 50 deg and above 60 deg
+#[ ${THERM%?} -ge 50 ] && echo "FF8000"
+#[ ${THERM%?} -ge 60 ] && echo "FF8000"
