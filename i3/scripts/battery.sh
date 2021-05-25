a803700d4fc231ce65a1727aeb40f71c07fbc71a
Vivek Thazhathattil <vivek.thazhathattil@gmail.com>
2021-05-26 01:01:07 +0530

    changes

--- i3/scripts/battery.sh
+++ i3/scripts/battery.sh
@@ -6,6 +6,12 @@ BAT=$(acpi -b | grep -E -o '[0-9][0-9][0-9]?' | head -n1)
 [ "$BAT" -le 90 ] && echo "  $BAT% "
 [ "$BAT" -gt 90 ] && echo "  $BAT% "
 
+STATUS=$(cat /sys/class/power_supply/BAT0/status)
+#if [ "$STATUS" != "Charging" || "$STATUS" != "Unknown" ]
+if [ "$STATUS" != "Charging" || "$STATUS" != "Full" ]
+then i3lock --fuzzy -r 2 -o -l
+fi
+
 # Set urgent flag below 5% or use orange below 20%
 #[ ${BAT%?} -le 5 ] && exit 33
 #[ ${BAT%?} -le 20 ] && echo "#FF8000"
--- i3/scripts/message.sh
+++ i3/scripts/message.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#printf "Trust no one, nor a zero. Both lie.\t\t\t\t\t\t\t\n"
-printf "!! Stay away from phonies !!\t\t\t\t\t\t\t\n"
+#printf "!! Stay away from phonies !!\t\t\t\t\t\t\t\n"
